c5bce9c424b6a6d57c1c72d999650e67af4d4a74
Naveen Mathew <pg13s_nathan@mandevian.com>
2019-04-13 21:52:30 -0500

    Minor modifications preprocessing code. Adding contents to README.md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Kepler
-STAT 430 project repository
+
+## Objective
+
+The objective of this project is to identify planetary transit using data from Kepler telescope. Classical methods include BLS (Box Least Squares) and TLS (Transit Least Squares). Modern methods apply machine learning to identify planetary transit. These modern methods take specific steps to remove eclipsing binary star systems from the transits.
+
+This project treats planetary transits as anomalies and attempts to detect them in an unsupervised way. Therefore, the scope of this project is limited to applying unsupervised deep learning to identify candidate planetary transits and to plot them. As a conclusion, this project is expected to address cases of correct and incorrect detections and to propose ways to address them, which may develop into a long-term project.
+
+## Platform and Tools
+
+The code has been tested on the following platforms:
+
+**Platform 1:**
+
+- Ubuntu 16.04 (shell, sed, grep, top, wget, head, awk)
+- R version 3.5.2
+
+**Platform 2:**
+
+- R Studio Cloud
+- R version 3.5.1
--- preprocess.R
+++ preprocess.R
@@ -12,35 +12,65 @@ text <- data.frame(text)
 text$X2 <- as.numeric(text$X2)
 
 for(file in files) {
-  mse <- rep(0, 9999)
-  for(i in 2:10000) {
-    print(i)
-    tser <- ts(data = temp$RESIDUAL_FLUX, frequency = i)
-    dcomp <- decompose(tser)
-    mse[i-1] <- mean(dcomp$random^2, na.rm = T)
-  }
-  plot(2:(length(mse)-1), mse)
+  # mse <- rep(0, 9999)
+  # for(i in 2:10000) {
+  #   print(i)
+  #   tser <- ts(data = temp$RESIDUAL_FLUX, frequency = i)
+  #   dcomp <- decompose(tser)
+  #   mse[i-1] <- mean(dcomp$random^2, na.rm = T)
+  # }
+  # plot(2:(length(mse)-1), mse)
   temp <- data.frame(read_delim(file, delim = " ", skip = 35, col_names = F),
                      stringsAsFactors = F)
   temp$X8 <- NULL
   temp <- data.frame(sapply(temp, as.numeric))
-  cols <- read_lines("data/kplr001026957_q1_q16_tce_01_dvt_lc.tbl", skip = 31,
-                     n_max = 1)
+  cols <- read_lines(file, skip = 31, n_max = 1)
   cols <- strsplit(cols, "\\ {0,}\\|")[[1]][-c(1)]
   colnames(temp) <- cols
-  temp$RESIDUAL_FLUX <- na.interpolation(temp$RESIDUAL_FLUX)
-  fft_res <- fft(temp$RESIDUAL_FLUX)
-  x <- Re(fft_res)
-  y <- Im(fft_res)
-  df <- data.frame(re = x, im = y)
+  # temp$RESIDUAL_FLUX <- na.interpolation(temp$RESIDUAL_FLUX)
+  # fft_res <- fft(temp$RESIDUAL_FLUX)
+  # x <- Re(fft_res)
+  # y <- Im(fft_res)
+  # df <- data.frame(re = x, im = y)
+  tsteps <- 1
+  batch_size   <- 40
+  epochs       <- 300
+  lag_setting <- 1
   model <- keras_model_sequential()
-  model %>%
-    layer_dense(units = 15, activation = "tanh", input_shape = ncol(x)) %>%
-    layer_dense(units = 10, activation = "tanh") %>%
-    layer_dense(units = 15, activation = "tanh") %>%
-    layer_dense(units = ncol(x_train))
   
+  model %>%
+    layer_cudnn_lstm(units            = 50, 
+               input_shape      = c(tsteps, 1), 
+               batch_size       = batch_size,
+               return_sequences = TRUE, 
+               stateful         = F) %>% 
+    layer_cudnn_lstm(units            = 50, 
+               return_sequences = FALSE, 
+               stateful         = F) %>% 
+    layer_dense(units = 1)
+  model %>% 
+    compile(loss = 'mae', optimizer = 'adam')
   summary(model)
+  lag_tbl <- temp %>%
+    mutate(value_lag = lag(RESIDUAL_FLUX, n = lag_setting)) %>%
+    filter(!is.na(value_lag))
+  x_train_vec <- lag_tbl$value_lag
+  x_train_arr <- array(data = x_train_vec, dim = c(length(x_train_vec), 1, 1))
+  y_train_vec <- lag_tbl$value
+  y_train_arr <- array(data = y_train_vec, dim = c(length(y_train_vec), 1))
+  for (i in 1:epochs) {
+    model %>% fit(x          = x_train_arr, 
+                  y          = y_train_arr, 
+                  batch_size = batch_size,
+                  epochs     = 1, 
+                  verbose    = 1, 
+                  shuffle    = FALSE)
+    
+    model %>% reset_states()
+    cat("Epoch: ", i)
+    
+  }
+  
   plot(y ~ x, df)
   plot(x = temp$TIME, y = x)
   plot(x = temp$TIME, y = y)
