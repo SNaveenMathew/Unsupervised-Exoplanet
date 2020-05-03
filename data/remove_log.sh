43479d79085ec21fc3f180cc7c3e193f108815bb
Naveen.Sathiyanathan <naveen.sathiyanathan@sanofi.com>
2020-05-03 19:18:09 -0400

    1) Ignoring .DS_Store, 2) Added Mac OSX support for sed script

--- data/remove_log.sh
+++ data/remove_log.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sed -i 's/\ \-a\ .*\.log$//g' Kepler_KOI_DV_wget.bat
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed -i "" 's/\ \-a\ .*\.log$//g' Kepler_KOI_DV_wget.bat
+else
+	sed -i 's/\ \-a\ .*\.log$//g' Kepler_KOI_DV_wget.bat
+fi
