ed13b59f07a18606373047bfbf140f8d8e2bf6a7
SNaveenMathew <naveenmathewnathan.s@gmail.com>
2026-08-29 17:26:58 -0400

    feat(cli): enhance main.R with configurable batch size, sequence length, and epochs

--- main.R
+++ main.R
@@ -1,23 +1,42 @@
-# Note: Keras uses multiple cores. Therefore multiprocessing is not used
+# Main entry point for Unsupervised Exoplanet Detection
 
 library(readr)
-library(imputeTS)
 library(reticulate)
-reticulate::use_condaenv("tf_gpu")
+# Attempt to use tf_gpu conda env if present
+tryCatch({
+  reticulate::use_condaenv("tf_gpu", required = FALSE)
+}, error = function(e) {
+  # Fall back to default python/keras environment
+})
+
 library(keras)
-library(kerasR)
 library(reshape2)
 library(dplyr)
 library(docopt)
 library(RSQLite)
 source("util.R")
 source("pipeline.R")
-doc <- "Usage: main.R [--PATH PATH]
 
---PATH=PATH       Relative path of folder containing data [default: data]"
+doc <- "Usage: main.R [--PATH=<data_dir>] [--seq_len=<len>] [--batch_size=<bs>] [--epochs=<ep>] [--run_hrs=<hrs>] [--db=<db_file>]
+
+Options:
+  --PATH=<data_dir>      Relative path of folder containing light curves [default: data]
+  --seq_len=<len>        Sequence window length in cadences [default: 128]
+  --batch_size=<bs>      Batch size for GPU training [default: 256]
+  --epochs=<ep>          Training epochs per star/batch [default: 20]
+  --run_hrs=<hrs>        Maximum pipeline runtime in hours [default: 8]
+  --db=<db_file>         SQLite database output path [default: shiny/exoplanet_db.sqlite]
+"
 
 opt <- docopt(doc)
 
-set.seed(1)
+set.seed(42)
 
-run_pipeline(opt$PATH)
+run_pipeline(
+  data_dir   = opt$PATH,
+  seq_len    = as.integer(opt$seq_len),
+  batch_size = as.integer(opt$batch_size),
+  epochs     = as.integer(opt$epochs),
+  run_hrs    = as.numeric(opt$run_hrs),
+  db_file    = opt$db
+)
